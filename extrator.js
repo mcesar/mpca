6cf3aac066763cd8f1e39a4c6260aba5d650f09e
marcosco <marcos-cesar.oliveira@planejamento.gov.br>
2014-11-06 16:47:40 -0200

    Adição de outros repositórios git além do SIOP

--- extrator.js
+++ extrator.js
@@ -1,30 +1,137 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
 var db = require('./db');
+var argv = require('optimist').argv;
 
 var entidades = [];
 var mapaDeEntidades = {};
 
-var commits = fs.readFileSync(__dirname + '/log.csv').toString().split('\n');
+var repositorioName = argv._[0] || 'siop';
+
+var repositorioMap = { siop: 1, derby: 2, hadoop: 3, wildfly: 4, 'eclipse.platform.ui': 5, 'eclipse.jdt': 6, 'geronimo': 7, 'lucene': 8 }
+
+var repositorio = repositorioMap[repositorioName];
+
+var dir = './../../' + repositorioName + '-historage';
+
+console.log(repositorioName, repositorio, dir);
+
+var issueExtractor = {
+	siop: function (comment) {
+		var result = comment.match(/#(\d+)/g), i;
+		if (!!result) {
+			for (i = 0; i < result.length; i += 1) {
+				result[i] = result[i].substring(1)
+			}
+		}
+		return result;
+	},
+	derby: function (comment) {
+		var result = comment.match(/derby-(\d+)/gi), i;
+		if (!!result) {
+			for (i = 0; i < result.length; i += 1) {
+				result[i] = result[i].substring(6)
+			}			
+		}
+		return result;
+	},
+	hadoop: function (comment) {
+		var sufix = {hadoop: "0", mapreduce: "1", hdfs: "2", yarn: "3"};
+		var result = comment.match(/(hadoop|mapreduce|hdfs|yarn)-(\d+)/gi), i;
+		if (!!result) {
+			for (i = 0; i < result.length; i += 1) {
+				var arr = result[i].split('-')
+				result[i] = arr[1] + sufix[arr[0]]
+			}			
+		}
+		return result;
+	},
+	wildfly: function (comment) {
+		var sufix = {wfly: "0", jbpapp: "1", jbqa: "2", as7: "3", jbas: "4"};
+		var result = comment.match(/(wfly|jbpapp|jbqa|as7|jbas)-(\d+)/gi), i;
+		if (!!result) {
+			for (i = 0; i < result.length; i += 1) {
+				var arr = result[i].split('-')
+				result[i] = arr[1] + sufix[arr[0]]
+			}			
+		}
+		return result;
+	},
+	'eclipse.platform.ui': function (comment) {
+		var result = comment.match(/(bug|fix|fix for)\ +(\d+)/gi), i;
+		if (!!result) {
+			for (i = 0; i < result.length; i += 1) {
+				var arr = result[i].split(' ')
+				result[i] = arr[arr.length-1]
+			}			
+		}
+		return result;
+	},
+	'eclipse.jdt': function (comment) {
+		var result = comment.match(/(bug|fix|fix for)\ +(\d+)/gi), i;
+		if (!result) {
+			result = comment.match(/^(\d+)$/gi)
+		}
+		if (!!result) {
+			for (i = 0; i < result.length; i += 1) {
+				var arr = result[i].split(' ')
+				result[i] = arr[arr.length-1]
+			}			
+		}
+		return result;
+	},
+	geronimo: function (comment) {
+		var result = comment.match(/geronimo-(\d+)/gi), i;
+		if (!!result) {
+			for (i = 0; i < result.length; i += 1) {
+				result[i] = result[i].substring(9)
+			}			
+		}
+		return result;
+	},
+	lucene: function (comment) {
+		var sufix = {lucene: "0", solr: "1"};
+		var result = comment.match(/(lucene|solr)-(\d+)/gi), i;
+		if (!!result) {
+			for (i = 0; i < result.length; i += 1) {
+				var arr = result[i].split('-')
+				result[i] = arr[1] + sufix[arr[0]]
+			}			
+		}
+		return result;
+	}
+}
+
+var commits = fs.readFileSync(dir + '/' + repositorioName + '.log.csv').toString().split('\n');
 
 console.log('Excluindo commits_issues');
-db.query('delete from commits_issues', function (err, results) {
-	if (err) { throw err; }
-	console.log('Excluindo commits_entidades');
-	db.query('delete from commits_entidades', function (err, results) {
+db.query('delete from commits_issues where commit in ' +
+			'(select id from commits where repositorio = ' + repositorio + ')', 
+	function (err, results) {
 		if (err) { throw err; }
-		console.log('Excluindo entidades');
-		db.query('delete from entidades', function (err, results) {
-			if (err) { throw err; }
-			console.log('Excluindo commits');
-			db.query('delete from commits', function (err, results) {
+		console.log('Excluindo commits_entidades');
+		db.query('delete from commits_entidades where commit in ' +
+			'(select id from commits where repositorio = ' + repositorio + ')', 
+			function (err, results) {
 				if (err) { throw err; }
-				console.log('Incluindo commits');
-				nextCommit(0);
-			});
-		});
-	});
-});
+				console.log('Excluindo entidades');
+				db.query('delete from entidades where repositorio = ' + repositorio, 
+					function (err, results) {
+						if (err) { throw err; }
+						console.log('Excluindo commits');
+						db.query('delete from commits where repositorio = ' + repositorio, 
+							function (err, results) {
+								if (err) { throw err; }
+								console.log('Incluindo commits');
+								nextCommit(0);
+							}
+						);
+					}
+				);
+			}
+		);
+	}
+);
 
 function nextCommit (i) {
 	if (i >= commits.length) {
@@ -39,19 +146,22 @@ function nextCommit (i) {
 	arr[2] = arr[2].replace(' +0000', '');
 	
 	var commit = arr[0];
-	var issues = arr[3].match(/#(\d+)/g);
-	var sql = 'insert into commits(id,autor,data,comentario) values (?,?,?,?)';
+	var issues = issueExtractor[repositorioName](arr[3]);
+	var sql = 'insert into commits(id,autor,data,comentario,repositorio) values (?,?,?,?,?)';
 
+	arr[2] = arr[2].replace(/ [\+|-]\d\d\d\d/, '');
+	arr[3] = arr[3].substring(0, 10000);
+	arr[4] = repositorio
 
 	db.query(sql, arr, function (err, result) {
-		if (err) { throw err; }
+		if (err) { console.log('--------------------->' + i); throw err; }
 		var sql = '', values = [], j;
 		if (!!issues) {
 			issues = objectDeDup(issues);
 			for (j = 0; j < issues.length; j += 1) {
 				sql += 'insert into commits_issues(commit,issue) values (?,?);'
 				values.push(commit);
-				values.push(parseInt(issues[j].substring(1)));
+				values.push(parseInt(issues[j]));
 			}			
 		}
 		if (sql.length === 0) {
@@ -66,7 +176,7 @@ function nextCommit (i) {
 
 	function coletaEntidades () {
 		var cmd = 'git diff-tree --no-commit-id --name-only -r ' + commit;
-		exec(cmd, {cwd:'../historage-data'}, function (err, stdout, stderr) {
+		exec(cmd, {cwd:dir}, function (err, stdout, stderr) {
 			var lines = stdout.split('\n'), j;
 			var line;
 			for (j = 0; j < lines.length; j += 1) {
@@ -94,11 +204,13 @@ function proximaEntidade (i) {
 	if (i % 100 === 0) {
 		process.stdout.write('.');
 	}
-	var sql = 'insert into entidades(caminho,tipo) values (?,?)'; 
-	db.query(sql, [entidades[i],tipoEntidade(entidades[i])], function (err, result) {
-		if (err) { throw err; }
-		proximoCommitEntidade(result.insertId, mapaDeEntidades[entidades[i]], 0);
-	});
+	var sql = 'insert into entidades(caminho,tipo,repositorio) values (?,?,?)'; 
+	db.query(sql, [entidades[i],tipoEntidade(entidades[i]),repositorio], 
+		function (err, result) {
+			if (err) { throw err; }
+			proximoCommitEntidade(result.insertId, mapaDeEntidades[entidades[i]], 0);
+		}
+	);
 
 	function proximoCommitEntidade (entidadeId, commits, j) {
 		if (j >= commits.length) {
