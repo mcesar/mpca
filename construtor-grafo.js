703a621853b23ce62ec4e6bdaa482fc511501ef9
Marcos César de Oliveira <mcesarhm@gmail.com>
2014-11-07 09:08:19 -0200

    Ajustes para gerar adequadamente o grafo para o Bunch

--- construtor-grafo.js
+++ construtor-grafo.js
@@ -1,10 +1,15 @@
+var argv = require('optimist').argv;
 var db = require('./db');
 
-console.log('obtendo commits_entidades');
+var repositorioName = argv._[0] || 'siop';
+
+var repositorioMap = { siop: 1, derby: 2, hadoop: 3, wildfly: 4, 'eclipse.platform.ui': 5, 'eclipse.jdt': 6, 'geronimo': 7, 'lucene': 8 }
+
+var repositorio = repositorioMap[repositorioName];
 
 var grafo = {}
 
-db.query('select * from mpca.commits_entidades order by commit', function (err, result) {
+db.query('select * from mpca.commits_entidades where commit in (select id from commits where repositorio = ' + repositorio + ') order by commit,entidade', function (err, result) {
 	if (err) { throw err; }
 	var entidades = [];
 	var commitAnterior;
@@ -16,24 +21,26 @@ db.query('select * from mpca.commits_entidades order by commit', function (err, 
 			commitAnterior = result[i].commit
 		}
 		entidades.push(result[i].entidade)
-		if (i % 1000 == 0) {
-			console.log(i + '***')
-		}
 	}
-	console.log(i + '***')
+	db.end()
 	coletaCombinacoes(entidades)
-	console.log(Object.keys(grafo).length + '<----------------')
+	var count = 0;
+	Object.keys(grafo).forEach(function(k) {
+		var arr = k.split('|')
+		if (grafo[k] > 1) {
+			console.log(arr[0] + ' ' + arr[1] + ' ' + grafo[k])
+		}
+	});
 });
 
 function coletaCombinacoes (entidades) {
 	if (entidades.length > 100) { return }
-	console.log(entidades.length)
 	var i, j, chave;
 	for (i = 0; i < entidades.length; i++) {
 		for (j = 0; j < entidades.length; j++) {
 			if (entidades[i] !== entidades[j]) {
 				chave = entidades[i] + '|' + entidades[j]
-				if (grafo[chave]) {
+				if (!grafo[chave]) {
 					grafo[chave] = 0
 				}
 				grafo[chave] = grafo[chave] + 1
