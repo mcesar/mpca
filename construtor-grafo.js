432eb98ebcc2755d8a5746ddb962227934dd73be
marcosco <marcos-cesar.oliveira@planejamento.gov.br>
2014-11-14 15:49:25 -0200

    Acrescenta opção à geração de grafo

--- construtor-grafo.js
+++ construtor-grafo.js
@@ -3,22 +3,28 @@ var db = require('./db');
 
 var repositorioName = argv._[0] || 'siop';
 
+var source = argv._[1] || 'commits';
+
 var repositorioMap = { siop: 1, derby: 2, hadoop: 3, wildfly: 4, 'eclipse.platform.ui': 5, 'eclipse.jdt': 6, 'geronimo': 7, 'lucene': 8 }
 
 var repositorio = repositorioMap[repositorioName];
 
+var sql = { 
+	commits: 'select commit as id, entidade from mpca.commits_entidades where commit in (select id from commits where repositorio = ?) order by commit,entidade', 
+	issues: 'select distinct issue as id, ce.commit, entidade from mpca.commits_entidades ce inner join mpca.commits_issues ci on ci.commit = ce.commit where ce.commit in (select id from commits where repositorio = ?) order by issue,entidade' }
+
 var grafo = {}
 
-db.query('select * from mpca.commits_entidades where commit in (select id from commits where repositorio = ' + repositorio + ') order by commit,entidade', function (err, result) {
+db.query(sql[source], [repositorio], function (err, result) {
 	if (err) { throw err; }
 	var entidades = [];
-	var commitAnterior;
+	var idAnterior;
 	var i;
 	for (i = 0; i < result.length; i += 1) {
-		if (result[i].commit !== commitAnterior) {
+		if (result[i].id !== idAnterior) {
 			coletaCombinacoes(entidades)
 			entidades = []
-			commitAnterior = result[i].commit
+			idAnterior = result[i].id
 		}
 		entidades.push(result[i].entidade)
 	}
@@ -34,7 +40,7 @@ db.query('select * from mpca.commits_entidades where commit in (select id from c
 });
 
 function coletaCombinacoes (entidades) {
-	if (entidades.length > 100) { return }
+	if (entidades.length > 200) { return }
 	var i, j, chave;
 	for (i = 0; i < entidades.length; i++) {
 		for (j = 0; j < entidades.length; j++) {
