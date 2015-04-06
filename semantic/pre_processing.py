92347bd231567027b5461074ef384c7868a398e4
marcosco <marcos-cesar.oliveira@planejamento.gov.br>
2015-04-06 11:01:10 -0300

    Fix code page and special characters on path problems

--- semantic/pre_processing.py
+++ semantic/pre_processing.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
 import sys
+import argparse
 import nltk
 from nltk.corpus import stopwords
 
-stop = stopwords.words('english')
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--code_page", default="utf-8")
+parser.add_argument("-l", "--language", default="english")
+args = parser.parse_args()
+
+stop = stopwords.words(args.language)
 
 porter = nltk.PorterStemmer()
 # [porter.stem(t) for t in tokens]
@@ -13,8 +19,9 @@ processed = set()
 
 for token in sys.stdin:
 	t = token.lower().strip()
+	t = unicode(t, args.code_page)
 	if t not in stop:
 		t = porter.stem(t)
 		if t not in processed and len(t) > 1 and not t.isdigit():
 			processed.add(t)
-			print(t)
+			print(t.encode('utf-8'))
