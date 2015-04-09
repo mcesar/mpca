b32274845de56b1f7398e634b07c599810f5ec3a
marcosco <marcos-cesar.oliveira@planejamento.gov.br>
2015-04-09 17:02:38 -0300

    Add option to compute average semantic similarity of clusters

--- semantic/average_similarity.py
+++ semantic/average_similarity.py
@@ -11,16 +11,27 @@ parser.add_argument("-s", "--source", default=".")
 parser.add_argument("-i", "--index_source", default=".")
 parser.add_argument("-p", "--prefix", default="corpus")
 parser.add_argument("-g", "--granularity", default="coarse")
-parser.add_argument("-m,", "--print_modules_count", default=False, action="store_true")
-parser.add_argument("-v,", "--verbose", default=False, action="store_true")
+parser.add_argument("-m", "--print_modules_count", default=False, action="store_true")
+parser.add_argument("-v", "--verbose", default=False, action="store_true")
+parser.add_argument("-c", "--clusters", default="")
+parser.add_argument("-n", "--new_path_prefix", default="")
 args = parser.parse_args()
 
 filepaths = filesystem.find(args.source,'*')
+if len(filepaths) == 0:
+	print('Source path not found', args.source)
+	exit()
 
 if args.granularity == 'coarse':
 	entity_types = ['CL','IN']
 else:
 	entity_types = ['MT','FE','CN']
+
+clusters = {}
+if args.clusters != "":
+	for cluster_entity in open(args.clusters):
+		arr = cluster_entity.split(',')
+		clusters[arr[9]] = arr[4]
 
 modules = {}
 for path in filepaths:
@@ -32,14 +43,24 @@ for path in filepaths:
 	entity_type = arr[-2:-1][0]
 	if not entity_type in entity_types: 
 		continue
-	# entities_paths.append(path)
-	module_path = '/'.join(arr[0:-3])
-	if module_path not in modules:
-		modules[module_path] = []
-	modules[module_path].append(path)
+	if args.clusters == "":
+		module_path = '/'.join(arr[0:-3])
+	else:
+		key = path.replace(args.source, '')
+		if key in clusters:
+			module_path = clusters[key]
+		else:
+			module_path = ''
+	if module_path != '':
+		if module_path not in modules:
+			modules[module_path] = []
+		modules[module_path].append(path)
 
 if args.print_modules_count:
-	print(len(modules))
+	s = 0
+	for v in modules.values():
+		s += len(v)
+	print(len(modules), s)
 	exit()
 
 path_index = {}
@@ -67,8 +88,12 @@ for (module,paths) in modules.items():
 		continue
 	sum = 0
 	for i, path1 in enumerate(paths):
+		if args.new_path_prefix != '':
+			path1 = path1.replace(args.source, args.new_path_prefix)
 		v = index.similarity_by_id(path_index[path1])
 		for j, path2 in enumerate(paths):
+			if args.new_path_prefix != '':
+				path2 = path2.replace(args.source, args.new_path_prefix)
 			if j >= i or path1 == path2:
 				continue
 			sum += v[path_index[path2]]
