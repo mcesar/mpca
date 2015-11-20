3eabdf2c8c54062496594e634bc8ab3414de0295
Marcos César de Oliveira <mcesarhm@gmail.com>
2015-11-19 23:14:49 -0200

    Add info about modules with min/max cohesion/elements

--- semantic/average_similarity.py
+++ semantic/average_similarity.py
@@ -20,99 +20,123 @@ args = parser.parse_args()
 
 filepaths = filesystem.find(args.source,'*')
 if len(filepaths) == 0:
-	print('Source path not found', args.source)
-	exit()
+        print('Source path not found', args.source)
+        exit()
 
 if args.granularity == 'coarse':
-	entity_types = ['CL','IN']
+        entity_types = ['CL','IN']
 else:
-	entity_types = ['MT','FE','CN']
+        entity_types = ['MT','FE','CN']
 
 clusters = {}
 if args.clusters != "":
-	for cluster_entity in open(args.clusters):
-		if '\"' in cluster_entity:
-			arr = cluster_entity.split('\"')
-			entity_path = arr[1]
-			arr = arr[0].split(',')
-			cluster_name = arr[4]
-		else:
-			arr = cluster_entity.split(',')
-			cluster_name = arr[4]
-			entity_path = arr[9]
-		clusters[entity_path.replace('/CM/','/MT/')] = cluster_name
+        for cluster_entity in open(args.clusters):
+                if '\"' in cluster_entity:
+                        arr = cluster_entity.split('\"')
+                        entity_path = arr[1]
+                        arr = arr[0].split(',')
+                        cluster_name = arr[4]
+                else:
+                        arr = cluster_entity.split(',')
+                        cluster_name = arr[4]
+                        entity_path = arr[9]
+                clusters[entity_path.replace('/CM/','/MT/')] = cluster_name
 
 modules = {}
 for path in filepaths:
-	if path.endswith('.c'):
-		continue
-	arr = path.split('/')
-	if len(arr) < 2:
-		continue
-	entity_type = arr[-2:-1][0]
-	if not entity_type in entity_types: 
-		continue
-	module_path = '/'.join(arr[0:-3])
-	if args.clusters != "":
-		key = path.replace(args.source, '')
-		if key in clusters:
-			if args.exclude_clusters:
-				module_path = ''
-			else:
-				module_path = clusters[key]
-		else:
-			if not args.exclude_clusters:
-				module_path = ''
-	if module_path != '':
-		if module_path not in modules:
-			modules[module_path] = []
-		modules[module_path].append(path)
+        if path.endswith('.c'):
+                continue
+        arr = path.split('/')
+        if len(arr) < 2:
+                continue
+        entity_type = arr[-2:-1][0]
+        if not entity_type in entity_types: 
+                continue
+        module_path = '/'.join(arr[0:-3])
+        if args.clusters != "":
+                key = path.replace(args.source, '')
+                if key in clusters:
+                        if args.exclude_clusters:
+                                module_path = ''
+                        else:
+                                module_path = clusters[key]
+                else:
+                        if not args.exclude_clusters:
+                                module_path = ''
+        if module_path != '':
+                if module_path not in modules:
+                        modules[module_path] = []
+                modules[module_path].append(path)
 
 if args.print_modules_count:
-	s = 0
-	for v in modules.values():
-		s += len(v)
-	print(len(modules), s)
-	exit()
+        s = 0
+        for v in modules.values():
+                s += len(v)
+        print(len(modules), s)
+        exit()
 
 path_index = {}
 i = 0
 for path in open(os.path.join(args.index_source, args.prefix + '.index')):
-	path_index[path.strip()] = i
-	i = i + 1
+        path_index[path.strip()] = i
+        i = i + 1
 
 index = similarities.Similarity.load(
-	os.path.join(args.index_source, args.prefix + '.sm'))
+        os.path.join(args.index_source, args.prefix + '.sm'))
 
 # index_arr = []
 # for row in index:
-# 	row_arr = []
-# 	index_arr.append(row_arr)
-# 	for col in row:
-# 		pass
-# 	 	row_arr.append(col)
+#       row_arr = []
+#       index_arr.append(row_arr)
+#       for col in row:
+#               pass
+#               row_arr.append(col)
 
 sum_m = 0
 count = 0
+min_cohesion = {'module':'','CCM':1, 'n':0, 'paths':[]}
+max_cohesion = {'module':'','CCM':0, 'n':0, 'paths':[]}
+min_elements = {'module':'','CCM':0, 'n':99999, 'paths':[]}
+max_elements = {'module':'','CCM':0, 'n':0, 'paths':[]}
 for (module,paths) in modules.items():
-	n = len(paths) 
-	if n <= 1:
-		continue
-	sum = 0
-	for i, path1 in enumerate(paths):
-		if args.new_path_prefix != '':
-			path1 = path1.replace(args.source, args.new_path_prefix)
-		v = index.similarity_by_id(path_index[path1])
-		for j, path2 in enumerate(paths):
-			if args.new_path_prefix != '':
-				path2 = path2.replace(args.source, args.new_path_prefix)
-			if j >= i or path1 == path2:
-				continue
-			sum += v[path_index[path2]]
-	CCM = sum/(n*(n-1)/2) #Conceptual Coesion of Module
-	if CCM < 0: CCM = 0
-	if args.verbose:
-		print(module, CCM)
-	sum_m += CCM
-	count += 1
-print(sum_m/count)
+        n = len(paths) 
+        if n <= 1:
+                continue
+        sum = 0
+        for i, path1 in enumerate(paths):
+                if args.new_path_prefix != '':
+                        path1 = path1.replace(args.source, args.new_path_prefix)
+                v = index.similarity_by_id(path_index[path1])
+                for j, path2 in enumerate(paths):
+                        if args.new_path_prefix != '':
+                                path2 = path2.replace(args.source, args.new_path_prefix)
+                        if j >= i or path1 == path2:
+                                continue
+                        sum += v[path_index[path2]]
+        CCM = sum/(n*(n-1)/2) #Conceptual Coesion of Module
+        if CCM < 0: CCM = 0
+        if args.verbose:
+                print(module, CCM)
+        if CCM < min_cohesion['CCM'] and CCM > 0:
+            min_cohesion['module'] = module
+            min_cohesion['CCM'] = CCM
+            min_cohesion['n'] = n
+            min_cohesion['paths'] = paths
+        if CCM > max_cohesion['CCM']:
+            max_cohesion['module'] = module
+            max_cohesion['CCM'] = CCM
+            max_cohesion['n'] = n
+            max_cohesion['paths'] = paths
+        if n < min_elements['n'] and CCM > 0:
+            min_elements['module'] = module
+            min_elements['CCM'] = CCM
+            min_elements['n'] = n
+            min_elements['paths'] = paths
+        if n > max_elements['n']:
+            max_elements['module'] = module
+            max_elements['CCM'] = CCM
+            max_elements['n'] = n
+            max_elements['paths'] = paths
+        sum_m += CCM
+        count += 1
+print(sum_m/count, min_cohesion, max_cohesion, min_elements, max_elements)
--- semantic/top_terms.py
+++ semantic/top_terms.py
@@ -16,116 +16,116 @@ args = parser.parse_args()
 
 clusters = {}
 for cluster_entity in open(args.clusters):
-        if '\"' in cluster_entity:
-                arr = cluster_entity.split('\"')
-                entity_path = arr[1]
-                arr = arr[0].split(',')
-                cluster_name = arr[4]
-        else:
-                arr = cluster_entity.split(',')
-                cluster_name = arr[4]
-                entity_path = arr[9]
-        if cluster_name not in clusters:
-                clusters[cluster_name] = []
-        clusters[cluster_name].append(entity_path.replace('/CM/','/MT/'))
+    if '\"' in cluster_entity:
+        arr = cluster_entity.split('\"')
+        entity_path = arr[1]
+        arr = arr[0].split(',')
+        cluster_name = arr[4]
+    else:
+        arr = cluster_entity.split(',')
+        cluster_name = arr[4]
+        entity_path = arr[9]
+    if cluster_name not in clusters:
+        clusters[cluster_name] = []
+    clusters[cluster_name].append(entity_path.replace('/CM/','/MT/'))
 
 if args.frequency:
-        keys = clusters.keys() 
-        # Collect frequencies
-        clusters_terms = {}
-        for key in keys:
-                terms = {}
-                clusters_terms[key] = terms
-                for path in clusters[key]:
-                        fn = os.path.join(args.source, path)
-                        if os.path.isfile(fn):
-                                for l in open(fn):
-                                        arr = l.split('\t')
-                                        if arr[0].strip() not in terms:
-                                                terms[arr[0].strip()] = 0       
-                                        terms[arr[0].strip()] = terms[arr[0].strip()] + int(arr[1].strip())
-                        else:
-                                print(fn)
-        average_frequency = {}
-        # Compute relevance of the terms comparing the frequency on all other clusters. See Kuhn (2007)
-        for key in keys:
-                for term in clusters_terms[key]:
-                        if term not in average_frequency:
-                                average_frequency[term] = 0
-                        average_frequency[term] = average_frequency[term] + clusters_terms[key][term]
-        for term in average_frequency:
-                average_frequency[term] = average_frequency[term] / len(clusters)
-        for key in keys:
-                for term in clusters_terms[key]:
-                        clusters_terms[key][term] = clusters_terms[key][term] - average_frequency[term]
-        # Print results
-        first = True
-        for key in keys:
-                if first:
-                        first = False
-                else:
-                        print("")
-                print("%s;" % key, end="")
-                tfirst = True
-                for k in sorted(clusters_terms[key], key=clusters_terms[key].get, reverse=True):
-                        if tfirst:
-                                tfirst = False
-                        else:
-                                print(",", end="")
-                        print("%s:%d" % (k, clusters_terms[key][k]), end="")
-else:
-        #Load LSI index
-        dictionary = corpora.Dictionary.load(
-                os.path.join(args.index_source, args.prefix + '.dict'))
-        lsi = models.LsiModel.load(
-                os.path.join(args.index_source, args.prefix + '-t.lsi'))
-        index_t = similarities.Similarity.load(
-                os.path.join(args.index_source, args.prefix + '-t.sm'))
+    keys = clusters.keys()
+    # Collect frequencies
+    clusters_terms = {}
+    for key in keys:
         terms = {}
-        i = 0
-        for term in open(os.path.join(args.index_source, args.prefix + '.terms')):
-                terms[i] = term
-                i = i + 1
-        first = True
-        sims = {}
-        # Compute similarities
-        for key in clusters:
-                # Build a query from cluster's terms
-                q = set()
-                for path in clusters[key]:
-                        fn = os.path.join(args.source, path)
-                        if os.path.isfile(fn):
-                                for l in open(fn):
-                                        q.add(l.strip())
-                # Perform query
-                sims[key] = list(enumerate(index_t[lsi[dictionary.doc2bow(q)]]))
-        # Compute averages
-        averages = {}
-        for key in clusters:
-                for s in sims[key]:
-                        if s[0] not in averages:
-                                averages[s[0]] = 0
-                        averages[s[0]] = averages[s[0]] + s[1]
-        for k in averages.keys():
-                averages[k] = averages[k] / len(clusters)
-        # Compute relevance of the terms comparing the frequency on all other clusters. See Kuhn (2007)
-        for key in clusters:
-                new_sims = {}
-                for s in sims[key]:
-                        new_sims[s[0]] = (s[0], s[1] - averages[s[0]])
-                sims[key] = new_sims.values()
-        # Print results
-        for key in clusters:
-                if first:
-                        first = False
-                else:
-                        print("")
-                print("%s;" % key, end="")
-                cluster_sims = sorted(sims[key], key=lambda item: -item[1])[0:100]
-                tfirst = True
-                for sim in cluster_sims:
-                        if tfirst:
-                                tfirst = False
-                        else:
-                                print(",", end="")
-                        print("%s:%s:%s" % (sim[0],terms[sim[0]].strip(), sim[1]), end="")
+        clusters_terms[key] = terms
+        for path in clusters[key]:
+            fn = os.path.join(args.source, path)
+            if os.path.isfile(fn):
+                for l in open(fn):
+                    arr = l.split('\t')
+                    if arr[0].strip() not in terms:
+                        terms[arr[0].strip()] = 0
+                    terms[arr[0].strip()] = terms[arr[0].strip()] + int(arr[1].strip())
+            else:
+                print(fn)
+    average_frequency = {}
+    # Compute relevance of the terms comparing the frequency on all other clusters. See Kuhn (2007)
+    for key in keys:
+        for term in clusters_terms[key]:
+            if term not in average_frequency:
+                average_frequency[term] = 0
+            average_frequency[term] = average_frequency[term] + clusters_terms[key][term]
+    for term in average_frequency:
+        average_frequency[term] = average_frequency[term] / len(clusters)
+    for key in keys:
+        for term in clusters_terms[key]:
+            clusters_terms[key][term] = clusters_terms[key][term] - average_frequency[term]
+    # Print results
+    first = True
+    for key in keys:
+            if first:
+                    first = False
+            else:
+                    print("")
+            print("%s;" % key, end="")
+            tfirst = True
+            for k in sorted(clusters_terms[key], key=clusters_terms[key].get, reverse=True):
+                    if tfirst:
+                            tfirst = False
+                    else:
+                            print(",", end="")
+                    print("%s:%d" % (k, clusters_terms[key][k]), end="")
+else:
+    #Load LSI index
+    dictionary = corpora.Dictionary.load(
+            os.path.join(args.index_source, args.prefix + '.dict'))
+    lsi = models.LsiModel.load(
+            os.path.join(args.index_source, args.prefix + '-t.lsi'))
+    index_t = similarities.Similarity.load(
+            os.path.join(args.index_source, args.prefix + '-t.sm'))
+    terms = {}
+    i = 0
+    for term in open(os.path.join(args.index_source, args.prefix + '.terms')):
+            terms[i] = term
+            i = i + 1
+    first = True
+    sims = {}
+    # Compute similarities
+    for key in clusters:
+            # Build a query from cluster's terms
+            q = set()
+            for path in clusters[key]:
+                    fn = os.path.join(args.source, path)
+                    if os.path.isfile(fn):
+                            for l in open(fn):
+                                    q.add(l.strip())
+            # Perform query
+            sims[key] = list(enumerate(index_t[lsi[dictionary.doc2bow(q)]]))
+    # Compute averages
+    averages = {}
+    for key in clusters:
+            for s in sims[key]:
+                    if s[0] not in averages:
+                            averages[s[0]] = 0
+                    averages[s[0]] = averages[s[0]] + s[1]
+    for k in averages.keys():
+            averages[k] = averages[k] / len(clusters)
+    # Compute relevance of the terms comparing the frequency on all other clusters. See Kuhn (2007)
+    for key in clusters:
+            new_sims = {}
+            for s in sims[key]:
+                    new_sims[s[0]] = (s[0], s[1] - averages[s[0]])
+            sims[key] = new_sims.values()
+    # Print results
+    for key in clusters:
+            if first:
+                    first = False
+            else:
+                    print("")
+            print("%s;" % key, end="")
+            cluster_sims = sorted(sims[key], key=lambda item: -item[1])[0:100]
+            tfirst = True
+            for sim in cluster_sims:
+                    if tfirst:
+                            tfirst = False
+                    else:
+                            print(",", end="")
+                    print("%s:%s:%s" % (sim[0],terms[sim[0]].strip(), sim[1]), end="")
